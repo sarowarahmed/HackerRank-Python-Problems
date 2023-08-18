e39e0783186a25a51c99207c15e000f6012f50af
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-08-18 12:57:39 +0530

    Added Arithmetic Operators

---
+++ Arithmetic Operators.py
@@ -0,0 +1,19 @@
+#Input Format:
+#The first line contains the first integer,a.
+#The second line contains the second integer,b.
+
+#Constraints:
+# 1<a<10^10
+# 1<b<10^10
+
+#Task
+#The first line contains the sum of the two numbers.
+#The second line contains the difference of the two numbers (first - second).
+#The third line contains the product of the two numbers.
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a + b)
+    print(a - b)
+    print(a*b)
