4abc94142b2ec9d61e120a753cca4bb20f13435b
Sarowar Ahmed <sectumsempra.cruciobliviate@gmail.com>
2023-08-20 23:53:52 +0530

    Added Loops

---
+++ Loops.py
@@ -0,0 +1,14 @@
+#Task
+#The provided code stub reads and integer, n, from STDIN. 
+#For all non-negative integers i<n, print i^2.
+
+#Input Format:
+#The first and only line contains the integer,n .
+
+#Constraints: 1<=n<=20
+
+#Output Format:
+#Print n lines, one corresponding to each i.
+
+n = int(input())
+print(*[num**2 for num in range(n)], sep='\n')
